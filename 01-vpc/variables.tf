4c7f992a539eb7db0735a52e47a9684da4ad315b
prudhvi <jamithireddyprudhvi9395@gmail.com>
2024-02-07 23:18:35 +0530

    prod

--- 01-vpc/variables.tf
+++ 01-vpc/variables.tf
@@ -22,15 +22,15 @@ variable "environment" {
 }
 
 variable "public_subnets_cidr" {
-  default = ["10.1.0.0/19", "10.1.32.0/19"]
+  default = ["10.1.1.0/24", "10.1.2.0/24"]
 }
 
 variable "private_subnets_cidr" {
-  default = ["10.1.64.0/19", "10.1.96.0/19"]
+  default = ["10.1.11.0/24", "10.1.12.0/24"]
 }
 
 variable "database_subnets_cidr" {
-  default = ["10.1.128.0/19", "10.1.160.0/19"]
+  default = ["10.1.21.0/24", "10.1.22.0/24"]
 }
 
 variable "is_peering_required" {
